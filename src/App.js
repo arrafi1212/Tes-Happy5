fc337449f846b53a988a7a578814d87e44595ffc
arrafi1212 <arrafifakri1@gmail.com>
2024-06-16 13:54:45 +0700

    Update and input multi data tasklist in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
-import React from "react";
-import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
+import Navbar from "./components/Navbar";
+import card1 from "./data/card1.json";
+import card2 from "./data/card2.json";
+import card3 from "./data/card3.json";
+import card4 from "./data/card4.json";
 
 function App() {
   return (
@@ -9,7 +12,12 @@ function App() {
       <Navbar />
       <div className="w-full p-10">
         <h1 className="font-semibold text-2xl mb-4">Product Roadmap</h1>
-        <TaskList />
+        <div className="List-task flex gap-4 w-100">
+          <TaskList data={card1} />
+          <TaskList data={card2} />
+          <TaskList data={card3} />
+          <TaskList data={card4} />
+        </div>
       </div>
     </div>
   );
