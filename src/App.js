8b7a5a4f45686b555b1e12ef7a9aa09de4cbd58b
arrafi1212 <arrafifakri1@gmail.com>
2024-06-16 13:49:17 +0700

    Add TaskList for list data section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Navbar />
+    <div className="App flex">
+      <Navbar />
+      <div className="w-full p-10">
+        <h1 className="font-semibold text-2xl mb-4">Product Roadmap</h1>
+        <TaskList />
       </div>
     </div>
   );
